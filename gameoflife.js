a7a8d3593dfc74e9f5b7c4a431e0e641494ff9ad
Chuchu <you@example.com>
2023-03-02 10:50:13 +0800

    update

---
+++ gameoflife.js
@@ -0,0 +1,178 @@
+const unitLength = 20;
+let boxColor = 150;
+let strokeColor = 50;
+let columns; /* To be determined by window width */
+let rows; /* To be determined by window height */
+let currentBoard;
+let nextBoard;
+let stopFlag = true;
+let reset = document.querySelector("#reset-game");
+let gameControl = document.querySelector("#game-control");
+
+function setup() {
+  /* Set the canvas to be under the element #canvas*/
+  const canvas = createCanvas(windowWidth, windowHeight - 100);
+  canvas.parent(document.querySelector("#canvas"));
+
+  /*Calculate the number of columns and rows */
+  columns = floor(width / unitLength);
+  rows = floor(height / unitLength);
+
+  /*Making both currentBoard and nextBoard 2-dimensional matrix that has (columns * rows) boxes. */
+  currentBoard = [];
+  nextBoard = [];
+  for (let i = 0; i < columns; i++) {
+    currentBoard[i] = [];
+    nextBoard[i] = [];
+  }
+  // Now both currentBoard and nextBoard are array of array of undefined values.
+  init(); // Set the initial values of the currentBoard and nextBoard
+}
+
+/**
+ * Initialize/reset the board state
+ */
+function init() {
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+      currentBoard[i][j] = 0;
+      nextBoard[i][j] = 0;
+    }
+  }
+}
+
+function draw() {
+  background(255);
+  generate();
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+      if (currentBoard[i][j] == 1) {
+        fill(boxColor);
+      } else {
+        fill(255);
+      }
+      stroke(strokeColor);
+      rect(i * unitLength, j * unitLength, unitLength, unitLength);
+    }
+  }
+}
+
+function generate() {
+  //Loop over every single box on the board
+  for (let x = 0; x < columns; x++) {
+    for (let y = 0; y < rows; y++) {
+      // Count all living members in the Moore neighborhood(8 boxes surrounding)
+      let neighbors = 0;
+      for (let i of [-1, 0, 1]) {
+        for (let j of [-1, 0, 1]) {
+          if (i == 0 && j == 0) {
+            // the cell itself is not its own neighbor
+            continue;
+          }
+          // The modulo operator is crucial for wrapping on the edge
+          neighbors +=
+            currentBoard[(x + i + columns) % columns][(y + j + rows) % rows];
+        }
+      }
+
+      // Rules of Life
+      if (currentBoard[x][y] == 1 && neighbors < 2) {
+        // Die of Loneliness
+        nextBoard[x][y] = 0;
+      } else if (currentBoard[x][y] == 1 && neighbors > 3) {
+        // Die of Overpopulation
+        nextBoard[x][y] = 0;
+      } else if (currentBoard[x][y] == 0 && neighbors == 3) {
+        // New life due to Reproduction
+        nextBoard[x][y] = 1;
+      } else {
+        // Stasis
+        nextBoard[x][y] = currentBoard[x][y];
+      }
+    }
+  }
+
+  // Swap the nextBoard to be the current Board
+  [currentBoard, nextBoard] = [nextBoard, currentBoard];
+}
+
+/**
+ * When mouse is dragged
+ */
+function mouseDragged() {
+  /**
+   * If the mouse coordinate is outside the board
+   */
+  if (mouseX > unitLength * columns || mouseY > unitLength * rows) {
+    return;
+  }
+  const x = Math.floor(mouseX / unitLength);
+  const y = Math.floor(mouseY / unitLength);
+  currentBoard[x][y] = 1;
+  fill(boxColor);
+  stroke(strokeColor);
+  rect(x * unitLength, y * unitLength, unitLength, unitLength);
+}
+
+/**
+ * When mouse is pressed
+ */
+function mousePressed() {
+  noLoop();
+  mouseDragged();
+}
+
+/**
+ * When mouse is released
+ */
+// function mouseReleased() {
+//   if (stopFlag == false) {
+//     loop();
+//   }
+// }
+
+//reset the game
+
+reset.addEventListener("click", function () {
+  init();
+  stopFlag = true;
+  loop();
+  gameControl.innerHTML = "start";
+});
+
+//start/pause the game
+
+gameControl.addEventListener("click", function () {
+  if (stopFlag == false) {
+    noLoop();
+    gameControl.innerHTML = "start";
+    stopFlag = true;
+  } else {
+    loop();
+    stopFlag = false;
+    gameControl.innerHTML = "pause";
+  }
+});
+
+//speed control
+// simulation Speed
+// slider html
+
+//Random initial states
+
+//change rules of survival
+
+//change rules of reproduction
+
+//Darken colors for stable life
+
+//multiple colors of life on the same board
+
+//Well-known patterns of Game of Life to select from (Examples: Gosper Glider Gun, Glider, Lightweight train)
+
+//Use Keyboard to control the cursor to place the life
+
+//Resize board on windows resize (Check out windowsResized())
+
+//Switching between different styles??
+
