2a8e74f64102c64606643d8b486e813e93802b47
Chuchu <you@example.com>
2023-03-03 20:23:34 +0800

    update

--- gameoflife.js
+++ gameoflife.js
@@ -1,5 +1,6 @@
 const unitLength = 20;
 let boxColor = "#000000";
+let stableLifeColor = "";
 let strokeColor = 50;
 let columns; /* To be determined by window width */
 let rows; /* To be determined by window height */
@@ -15,15 +16,24 @@ let reset = document.querySelector("#reset-game");
 let gameControl = document.querySelector("#game-control");
 let random = document.querySelector("#random");
 let color = document.querySelector("#colorInput");
+let stableColor = document.querySelector("#stableColorInput");
 let fpsRange = document.querySelector("#fpsRange");
 let aBtn = document.querySelector("#underPopulation");
 let bBtn = document.querySelector("#overPopulation");
 let cBtn = document.querySelector("#reproduction");
+let body = document.body;
+let colorMode = document.querySelector(".color-mode");
+let darkButton = document.querySelector(".dark-button");
+let lightButton = document.querySelector(".light-button");
+let notInCanvas = "false";
 
 function setup() {
-  console.log("setup", windowWidth, windowHeight);
+  let gameBoard = select(".gameBoard");
+
+  console.log("setup", gameBoard.width, gameBoard.height);
+
   /* Set the canvas to be under the element #canvas*/
-  const canvas = createCanvas(windowWidth, windowHeight - 100);
+  const canvas = createCanvas(gameBoard.width, gameBoard.height);
   canvas.parent(document.querySelector("#canvas"));
 
   /*Calculate the number of columns and rows */
@@ -95,7 +105,12 @@ function draw() {
   for (let i = 0; i < columns; i++) {
     for (let j = 0; j < rows; j++) {
       if (currentBoard[i][j] == 1) {
-        fill(boxColor);
+        //Darken colors for stable life
+        if (nextBoard[i][j] == 1) {
+          fill(stableLifeColor);
+        } else {
+          fill(boxColor);
+        }
       } else {
         fill(255);
       }
@@ -123,7 +138,6 @@ function generate() {
         }
       }
 
-      
       // Rules of Life
       if (currentBoard[x][y] == 1 && neighbors < underPopulation) {
         // Die of Loneliness
@@ -163,10 +177,9 @@ function mouseDragged() {
   rect(x * unitLength, y * unitLength, unitLength, unitLength);
   // chosen by the color-picker
   colorInput.addEventListener("click", () => {
-    document = colorInput.value;
+    boxColor = colorInput.value;
   });
 }
-
 /**
  * When mouse is pressed
  */
@@ -174,6 +187,7 @@ function mousePressed() {
   noLoop();
   mouseDragged();
 }
+
 
 /**
  * When mouse is released
@@ -232,10 +246,27 @@ cBtn.addEventListener("change", (event) => {
   reproduction = parseInt(result);
 });
 
+stableColorInput.addEventListener("change", () => {
+  stableLifeColor = stableColorInput.value;
+});
 
+//Switching between different styles
+function changeMode() {
+  body.classList.toggle("dark-mode");
+  lightButton.classList.toggle("invisible");
+  darkButton.classList.toggle("invisible");
+  // colorMode.innerHTML = "bright-mode";
+}
 
+body.addEventListener("click", (event) => {
+  let inCanvas = event.target.getAttribute("id");
+  if (inCanvas !== null) {
+    notInCanvas = false;
+  } else {
+    notInCanvas = true;
+  }
+});
 
-//Darken colors for stable life
 
 //multiple colors of life on the same board
 
@@ -243,4 +274,3 @@ cBtn.addEventListener("change", (event) => {
 
 //Use Keyboard to control the cursor to place the life
 
-//Switching between different styles??
